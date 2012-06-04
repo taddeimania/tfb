c3e747cd84d4ee5650c9f70dd6ff58470e97d36b
Joel Taddei <jtaddei@gmail.com>
2012-06-03 23:03:33 -0500

    large refactor

--- top_player_list/views.py
+++ top_player_list/views.py
@@ -6,61 +6,66 @@ from myproject.players import models as player_models
 import logic
 
 def get_top_ten_for_pos(posid, user=None):
-    CACHE_TIMEOUT = 360
+    CACHE_TIMEOUT = 1
     top_pos = "top{}s".format(posid)
     top_list = []
     if not user:
         search_criteria = player_models.Player.objects.filter(pos__startswith=posid.upper())
     else:
-        search_criteria = logic.avail_list(user, 'QB')
-    if not cache.get(top_pos):
-        try:
-            for player in search_criteria:
-                top_list.append(player.SeasonTotal())
-                top_list.sort(key=lambda x: x.Allfanpts, reverse=True)
-            top_list = top_list[:10]
-            cache.set(top_pos, top_list, CACHE_TIMEOUT)
-            return top_list
-        except Exception:
-            return
-    else:
-        return cache.get(top_pos)
+        search_criteria = logic.avail_list(user, posid)
+    #if not cache.get(top_pos):
+    try:
+        for player in search_criteria:
+            top_list.append(player.SeasonTotal())
+            top_list.sort(key=lambda x: x.Allfanpts, reverse=True)
+        top_list = top_list[:10]
+        #cache.set(top_pos, top_list, CACHE_TIMEOUT)
+        return top_list
+    except Exception:
+        return
+    #else:
+    #    return cache.get(top_pos)
+
+
+def get_player_stats(arg, playerstats):
+    try:
+        playerobj = player_models.Player.objects.get(pk=arg)
+        for player in player_models.Stats.objects.filter(player=arg):
+            playerstats.append(player)
+            playerstats.sort(key=lambda x: x.week, reverse=False)
+        season_total = playerobj.SeasonTotal()
+        return playerobj, season_total
+    except player_models.Player.DoesNotExist:
+        pass
+
+
 
 def playerpage(request, arg=None):
     """pylint """
     playerstats = []
-    topqbs = []
-    toprbs = []
-    topwrs = []
-    toptes = []
-    topks = []
+    pos_list = ['QB', 'RB', 'WR', 'TE', 'K']
+    player_list = []
 
     player = request.POST.get('playersearch','')
     players = simplejson.dumps([x.player_name for x in player_models.Player.objects.all()])
 
     if str(arg).upper() == 'TOP':
         top = True
-        topqbs = get_top_ten_for_pos('QB')
-        toprbs = get_top_ten_for_pos('RB')
-        topwrs = get_top_ten_for_pos('WR')
-        toptes = get_top_ten_for_pos('TE')
-        topks = get_top_ten_for_pos('K')
+        for pos in pos_list:
+            player_list.append((pos, get_top_ten_for_pos(pos)))
 
     elif str(arg).upper() == 'TOPAVAIL':
         top = True
         avail = True
-        user = request.user
-        topqbs = get_top_ten_for_pos('QB', user=user)
-        toprbs = get_top_ten_for_pos('RB', user=user)
-        topwrs = get_top_ten_for_pos('WR', user=user)
-        toptes = get_top_ten_for_pos('TE', user=user)
-        topks = get_top_ten_for_pos('K', user=user)
+        for pos in pos_list:
+            player_list.append((pos, get_top_ten_for_pos(pos, user=request.user)))
 
     elif str(arg).upper() in base_views.POS_LIST:
         pos = True
         for player in player_models.Player.objects.filter(pos__startswith=str(arg).upper):
             playerstats.append(player.SeasonTotal())
         playerstats.sort(key=lambda x: x.Allfanpts, reverse=True)
+
     elif not arg:
         search = True
         if player:
@@ -71,25 +76,11 @@ def playerpage(request, arg=None):
                 arg = player.id
             except player_models.Player.DoesNotExist:
                 pass
-            try:
-                playerobj = player_models.Player.objects.get(pk=arg)
-                for player in player_models.Stats.objects.filter(player=arg):
-                    playerstats.append(player)
-                    playerstats.sort(key=lambda x: x.week, reverse=False)
-            except player_models.Player.DoesNotExist:
-                pass
+            playerobj, season_total = get_player_stats(arg, playerstats)
         pass
     else:
         playersearch = True
-        try:
-            playerobj = player_models.Player.objects.get(pk=arg)
-            for player in player_models.Stats.objects.filter(player=arg):
-                playerstats.append(player)
-                playerstats.sort(key=lambda x: x.week, reverse=False)
-        except player_models.Player.DoesNotExist:
-            pass
-
-
+        playerobj, season_total = get_player_stats(arg, playerstats)
     return base_views.default_response(locals(), request, 'base_playerpage_vars.html')
 
 # WIP!
