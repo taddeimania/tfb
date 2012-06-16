be74ad4b0315ab27f998baea54175bf1d6c018fe
ubuntu <jtaddei@gmail.com>
2012-06-16 18:34:37 -0500

    calm before another big refactor

--- player_card/views.py
+++ player_card/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.base import TemplateView
-from players import models as player_models
+from myproject.players import models as player_models
 from myproject.views import default_response
-from utility import logic
+from myproject.utility import logic
 
 class PlayerPageView(TemplateView):
     template_name = "base_player_vars.html"
