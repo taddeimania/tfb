fdfae859e30089c17042df3e49e8cc244ea2aced
qbeagles <jtaddei@gmail.com>
2012-07-04 11:33:11 -0500

    this should fix a lot of stupid web crawler emails i get

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
 from tfb import views as base_views
 from tfb.messages import views as message_views
 from tfb.player_card import views as player_card_views
@@ -16,20 +17,20 @@ urlpatterns = patterns('',
     url(r'^uteam/(?P<team_id>\w+)/$', base_views.NotMyTeamView.as_view(), name='uteam'),
     url(r'^about/$', base_views.about ,name='about'),
     url(r'^uteam/$', base_views.my_team_page,name='myteam'),
-    url(r'^myteam/$', base_views.my_team_page,name='myteam'),
+    url(r'^myteam/$', login_required(base_views.my_team_page),name='myteam'),
     url(r'^messages/$', message_views.MessageView.as_view(),name='message'),
-	url(r'^league/$', base_views.league_page,name='league'),
-    url(r'^league/(?P<week>\w+)/$', base_views.league_page,name='league'),
+	url(r'^league/$', login_required(base_views.league_page),name='league'),
+    url(r'^league/(?P<week>\w+)/$', login_required(base_views.league_page),name='league'),
     url(r'^leagueadmin/$', base_views.leagueadmin,name='leagueadmin'),
     url(r'^leagueadmin/(?P<arg>\w+)/$', base_views.leagueadmin,name='leagueadmin'),
 	url(r'^login/$', 'django.contrib.auth.views.login'),
 	url(r'^logout/$', base_views.logout_user,name='logout_user'),
-	url(r'^profile/$', base_views.ProfileView.as_view(),name='ProfileView'),
+	url(r'^profile/$', login_required(base_views.ProfileView.as_view()),name='ProfileView'),
     url(r'^profile/edit/$', base_views.profileedit,name='profileedit'),
 	url(r'^joinleague/$', base_views.joinleague,name='joinleague'),
 	url(r'^pickup/(?P<posid>\w+)/$', base_views.pickup,name='pickup'),
 	url(r'^list/(?P<posid>\w+)/$', base_views.list_player,name='list'),
-    url(r'^matchup/$', base_views.matchup_page,name='matchup'),
+    url(r'^matchup/$', login_required(base_views.matchup_page),name='matchup'),
     url(r'^draft/$', draft_views.draftpage, name='draftpage'),
     url(r'^drag/$', draft_views.drag_and_drop, name='draftpage'),
     url(r'^matchup/$', base_views.matchup_page,name='matchup'),
